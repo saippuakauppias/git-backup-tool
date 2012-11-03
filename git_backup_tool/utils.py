9e041c46de9ed5c1e5bf49d75c84e45305469221
Denis Veselov <progr-m@yandex.ru>
2012-11-03 11:46:36 +0400

    add function for get config from file

---
+++ git_backup_tool/utils.py
@@ -0,0 +1,10 @@
+from ConfigParser import ConfigParser
+
+
+def config_from_file(filename):
+    cfg = ConfigParser()
+    cfg.read(filename)
+    result = {}
+    for section in cfg.sections():
+        result.update({section: dict(cfg.items(section))})
+    return result
