b7e59029c938918691e30fe9ded150f32e31570a
Denis Veselov <progr-m@yandex.ru>
2012-11-05 00:06:00 +0400

    fix git commands

--- git_backup_tool/git.py
+++ git_backup_tool/git.py
@@ -2,26 +2,26 @@ from utils import run_command
 
 
 def git_init(directory):
-    print run_command(['git', 'init'], directory)
+    print run_command('git init', directory)
 
 
 def git_add_all(directory):
-    print run_command(['git', 'add', '.'], directory)
+    print run_command('git add .', directory)
 
 
 def git_status(directory):
-    return run_command(['git', 'status', '--short'], directory)
+    return run_command('git status --short', directory)
 
 
 def git_commit(directory, status):
-    print run_command(['git', 'commit', '-am', status], directory)
+    print run_command('git commit -am "{0}"'.format(status), directory)
 
 
 def git_remote_add(directory, git_remote):
-    remote_exists = run_command(['git', 'remote', 'show'], directory)
+    remote_exists = run_command('git remote show', directory)
     if not remote_exists:
-        print run_command(['git', 'remote', 'add', 'origin', git_remote])
+        print run_command('git remote add origin {0}'.format(git_remote))
 
 
 def git_push(directory):
-    print run_command(['git', 'push', '-u', 'origin', 'master'], directory)
+    print run_command('git push -u origin master', directory)
