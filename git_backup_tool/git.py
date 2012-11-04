9ff1052f5a865508789cb57500003028beb28a74
Denis Veselov <progr-m@yandex.ru>
2012-11-04 13:06:51 +0400

    add git commands

---
+++ git_backup_tool/git.py
@@ -0,0 +1,27 @@
+from utils import run_command
+
+
+def git_init(directory):
+    print run_command(['git', 'init'], directory)
+
+
+def git_add_all(directory):
+    print run_command(['git', 'add', '.'], directory)
+
+
+def git_status(directory):
+    return run_command(['git', 'status', '--short'], directory)
+
+
+def git_commit(directory, status):
+    print run_command(['git', 'commit', '-am', status], directory)
+
+
+def git_remote_add(directory, git_remote):
+    remote_exists = run_command(['git', 'remote', 'show'], directory)
+    if not remote_exists:
+        print run_command(['git', 'remote', 'add', 'origin', git_remote])
+
+
+def git_push(directory):
+    print run_command(['git', 'push', '-u', 'origin', 'master'])
